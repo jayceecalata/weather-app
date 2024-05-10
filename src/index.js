9826af4f4428697a758278dbae5c5502e801f67a
Jaycee Calata <132433212+jayceecalata@users.noreply.github.com>
2024-05-10 15:08:46 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/style.css';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log("Hello, you're in development mode!");
+}
---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname, './src/assets'),
+      },
+    ]
+  },    
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    pathinfo: false,
+    clean: true,
+  },
+};
