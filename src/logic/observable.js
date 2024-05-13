7a627647c9a44f61e2461f5fa4ff482fe962ebd4
Jaycee Calata <132433212+jayceecalata@users.noreply.github.com>
2024-05-13 17:22:06 +0800

    feat: create an observable class

---
+++ src/logic/observable.js
@@ -0,0 +1,20 @@
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(func) {
+    this.observers.push(func);
+  }
+
+  unsubscribe(func) {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
+
+const observable = new Observable();
+export default observable;
