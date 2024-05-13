e7be9c7821b228cdd07e96e1594c560bf11c6fac
Jaycee Calata <132433212+jayceecalata@users.noreply.github.com>
2024-05-14 00:59:11 +0800

    fix: use https instead of http

--- src/api/url-constants.js
+++ src/api/url-constants.js
@@ -1,5 +1,5 @@
 const urlConstants = {
-  base: 'http://api.weatherapi.com/v1',
+  base: 'https://api.weatherapi.com/v1',
   method: '/forecast.json',
   parameters: {
     key: 'b64063bd0c4a49a38ef210916240205',
